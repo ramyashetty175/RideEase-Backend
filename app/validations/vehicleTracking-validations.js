de8e3355591c9f03259895b1938e09728873ad58
ramyashetty175 <127672092+ramyashetty175@users.noreply.github.com>
2025-11-05 18:22:53 +0530

    fields added to vehicleTrackingvalidation

--- app/validations/vehicleTracking-validations.js
+++ app/validations/vehicleTracking-validations.js
@@ -4,7 +4,12 @@ const VehicleTrackingValidation = Joi.object({
     latitude: Joi.number().required(),
     longitude: Joi.number().required(),
     speed: Joi.number().required(),
-    status: Joi.string().required()
+    maxSpeed: Joi.number(),
+    avgSpeed: Joi.number(),
+    distanceTravelled: Joi.number(),
+    status: Joi.string().required(),
+    isLive: Joi.boolean(),
+    lastUpdatedAt: Joi.date(),
 })
 
 module.exports = VehicleTrackingValidation;
